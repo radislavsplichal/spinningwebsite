16f248255119e9855ff079fa671cc980b83c8869
radislavsplichal <radislavsplichal@gmail.com>
2015-01-01 20:50:54 +0100

    Slightly improved Visuals / Testing DW

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<header>
+    <meta charset="utf-8">
+    <meta name="description" content="AJ SPORTS CLUB OFFICAL PAGE">
+    <meta name="author" content="Radislav Splichal">
+
+</header>
+<body>
+<head>
+
+
+</head>
+<section>
+    <nav>
+        
+        
+        
+    </nav>
+
+    <article>
+        <?php include 'article.html';                ?>
+        
+    </article>
+
+
+</section> 
+<footer>
+
+
+
+
+</footer>
+
+
+
+</body>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
