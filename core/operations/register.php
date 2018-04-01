3233d935693b56203a859cf09dcce540a901437d
radislavsplichal <radislavsplichal@gmail.com>
2018-04-01 23:17:00 +0200

    new template for login and registration

---
+++ core/operations/register.php
@@ -0,0 +1,18 @@
+<?php
+
+include '../classes/loadComponents.php';
+
+$cus_name = $_POST['name'];
+$cus_surname = $_POST['surname'];
+$cus_username = $_POST['username'];
+$cus_password = $_POST['password'];
+$cus_email = $_POST['email'];
+
+$user = new User();
+$response = $user->createUser($cus_name,$cus_surname,$cus_email,$cus_password,$cus_username);
+echo $response;
+
+//header("Location:"."../index.php");
+//exit();
+
+?>
