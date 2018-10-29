706f750b4458fddac0ea0fb19a3bb45ed278b12e
radislavsplichal <radislavsplichal@gmail.com>
2018-10-29 06:55:11 +0100

    event

--- core/calendar_behaviour.js
+++ core/calendar_behaviour.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 		$(".calendar__day").click (function(){
 			$( ".calendar__day" ).hide();})
-			
-	
+		    
+		$(".calendar__day").click(function(){
+			$("#form1").toggle(); });
 	
  
 });
--- core/index.php
+++ core/index.php
@@ -11,9 +11,28 @@ echo $_SESSION['username'];
 echo $_SESSION['status'];
 ?>
 <head>
+<title>Reservation System 1.0</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="calendar_behaviour.js"></script>
 </head>
-<script type="text/javascript" src="calendar_behabiour.js"></script>
+
+<style>
+<!--
+
+-->
+#form1 {
+     display:none;
+}
+
+
+
+</style>
+
+
+
+
+
 <content>
 <div class="wrapper">
 <?php
@@ -23,5 +42,35 @@ $calendar = new Calendar();
 
 echo $calendar->show();
 ?>
+
+<form id="form1" action='operations/createEvent.php'>
+  <b>Title</b> <input type="text" name="title">
+    <br><br>
+  <b>Description </b><input type="text" name="description">
+    <br><br>
+  <b> Capacity</b>
+  	</b><input type="number" name="capacity">
+  <b>Preferred Color:</b>
+  <select name="price">
+    <option value="90">90</option>
+    <option value="150">150</option>
+  </select>
+    <br><br>
+  <b>Comment:</b>
+    <br>
+  <textarea name="comment">
+    Enter your comment here
+  </textarea>
+    <br><br>
+  <button type="button" id="submit">Submit</button>
+  </form>
+</form>
+
+
+
+
+
+
+
 </div>
 </content>
